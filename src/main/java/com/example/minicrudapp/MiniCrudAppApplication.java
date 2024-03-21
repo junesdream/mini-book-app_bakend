0dc78d188a2cab6cb5dc9533b24cbc8bdbf3f454
junesdream <norisori@gmail.com>
2024-03-22 00:56:11 +0100

    1. Commit

---
+++ src/main/java/com/example/minicrudapp/MiniCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.minicrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCrudAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minicrudapp/MiniCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minicrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
